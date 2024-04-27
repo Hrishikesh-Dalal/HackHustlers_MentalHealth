a5ffe0936535fc8b273ff0810f5144c04130c2a8
Keerti Mehta <keertimehta@Keertis-MacBook-Air.local>
2024-04-28 05:08:08 +0530

    second commit

--- hack_hustlers/lib/pages/home.dart
+++ hack_hustlers/lib/pages/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hack_hustlers/pages/navbar.dart';
 import 'package:hack_hustlers/pages/bottomNavBar.dart';
+import 'package:hack_hustlers/pages/sleep_tracker.dart';
+//import 'package:webview_flutter/webview_flutter.dart';
+
 
 
 class HomePage extends StatefulWidget {
@@ -20,12 +23,12 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color.fromARGB(200, 300 ,300, 200),
         title:Text('Mental Health') ,
 
-      
-
-      
-
       ),
-      
+      //body: SleepTrackerPage(),
+        /*body:WebView(
+          initialUrl: 'https://example.com/your-mini-game',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),*/
     );
     
   }
